70ec9b0fc69cab095f2557967a7e5b942d2a1ee3
Biranchi Parida <biranchi104@hotmail.com>
2025-01-30 18:12:57 +0530

    first stepp

---
+++ src/main/java/com/ecomtask/App.java
@@ -0,0 +1,13 @@
+package com.ecomtask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
